8966b27efa682f3a520de95b2acbeaab16cdfe92
mash1024 <15fi011@ms.dendai.ac.jp>
2020-08-07 04:25:11 +0900

    APIのHOST設定を追加

--- src/main/java/com/iota/iri/service/API.java
+++ src/main/java/com/iota/iri/service/API.java
@@ -753,6 +753,34 @@ public class API {
         IRIがどうやってAPIを受け付けているかをまず調べる
         それをcompassに導入してやり取りを目指す
     */
+        String name = configuration.isTestnet() ? IRI.TESTNET_NAME : IRI.MAINNET_NAME;
+        MilestoneViewModel milestone = MilestoneViewModel.first(tangle);
+        
+        return GetNodeInfoResponse.create(
+                name,
+                IotaUtils.getIriVersion(),
+                Runtime.getRuntime().availableProcessors(),
+                Runtime.getRuntime().freeMemory(),
+                System.getProperty("java.version"),
+
+                Runtime.getRuntime().maxMemory(),
+                Runtime.getRuntime().totalMemory(),
+                latestMilestoneTracker.getLatestMilestoneHash(),
+                latestMilestoneTracker.getLatestMilestoneIndex(),
+                
+                snapshotProvider.getLatestSnapshot().getHash(),
+                snapshotProvider.getLatestSnapshot().getIndex(),
+                
+                milestone != null ? milestone.index() : -1,
+                snapshotProvider.getLatestSnapshot().getInitialIndex(),
+                
+                neighborRouter.getConnectedNeighbors().size(),
+                txPipeline.getBroadcastStageQueue().size(),
+                System.currentTimeMillis(),
+                tipsViewModel.size(),
+                transactionRequester.numberOfTransactionsToRequest(),
+                features,
+                configuration.getCoordinator().toString());
     }
 
     /**
