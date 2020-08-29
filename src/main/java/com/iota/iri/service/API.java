f26e076c901efcf74efe1f4773e97fff57aa83c7
mash1024 <15fi011@ms.dendai.ac.jp>
2020-08-29 14:23:20 +0900

    修正

--- src/main/java/com/iota/iri/service/API.java
+++ src/main/java/com/iota/iri/service/API.java
@@ -781,6 +781,35 @@ public class API {
                 Socket echoSocket = new Socket("192.168.1.72", portNumber);
             ) {
                 log.info("[%s] :connected!\n", Thread.currentThread().getName());
+
+                String name = configuration.isTestnet() ? IRI.TESTNET_NAME : IRI.MAINNET_NAME;
+                MilestoneViewModel milestone = MilestoneViewModel.first(tangle);
+
+                return GetNodeInfoResponse.create(
+                        name,
+                        IotaUtils.getIriVersion(),
+                        Runtime.getRuntime().availableProcessors(),
+                        Runtime.getRuntime().freeMemory(),
+                        System.getProperty("java.version"),
+
+                        Runtime.getRuntime().maxMemory(),
+                        Runtime.getRuntime().totalMemory(),
+                        latestMilestoneTracker.getLatestMilestoneHash(),
+                        latestMilestoneTracker.getLatestMilestoneIndex(),
+                        
+                        snapshotProvider.getLatestSnapshot().getHash(),
+                        snapshotProvider.getLatestSnapshot().getIndex(),
+                        
+                        milestone != null ? milestone.index() : -1,
+                        snapshotProvider.getLatestSnapshot().getInitialIndex(),
+                        
+                        neighborRouter.getConnectedNeighbors().size(),
+                        txPipeline.getBroadcastStageQueue().size(),
+                        System.currentTimeMillis(),
+                        tipsViewModel.size(),
+                        transactionRequester.numberOfTransactionsToRequest(),
+                        features,
+                        configuration.getCoordinator().toString());
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -857,35 +886,7 @@ public class API {
 			e.printStackTrace();
         }
         */
-        
-        String name = configuration.isTestnet() ? IRI.TESTNET_NAME : IRI.MAINNET_NAME;
-        MilestoneViewModel milestone = MilestoneViewModel.first(tangle);
-
-        return GetNodeInfoResponse.create(
-                name,
-                IotaUtils.getIriVersion(),
-                Runtime.getRuntime().availableProcessors(),
-                Runtime.getRuntime().freeMemory(),
-                System.getProperty("java.version"),
-
-                Runtime.getRuntime().maxMemory(),
-                Runtime.getRuntime().totalMemory(),
-                latestMilestoneTracker.getLatestMilestoneHash(),
-                latestMilestoneTracker.getLatestMilestoneIndex(),
-                
-                snapshotProvider.getLatestSnapshot().getHash(),
-                snapshotProvider.getLatestSnapshot().getIndex(),
-                
-                milestone != null ? milestone.index() : -1,
-                snapshotProvider.getLatestSnapshot().getInitialIndex(),
-                
-                neighborRouter.getConnectedNeighbors().size(),
-                txPipeline.getBroadcastStageQueue().size(),
-                System.currentTimeMillis(),
-                tipsViewModel.size(),
-                transactionRequester.numberOfTransactionsToRequest(),
-                features,
-                configuration.getCoordinator().toString());
+    
     }
 
 /*
