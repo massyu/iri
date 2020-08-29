8c56f84866bbe5ac509fe311d0ee894c13ffca3a
mash1024 <15fi011@ms.dendai.ac.jp>
2020-08-29 15:21:10 +0900

    修正

--- src/main/java/com/iota/iri/service/API.java
+++ src/main/java/com/iota/iri/service/API.java
@@ -48,13 +48,21 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 //変更箇所
-import java.io.IOException;
-import java.net.InetSocketAddress;
-import java.nio.ByteBuffer;
-import java.nio.channels.SocketChannel;
+//import java.io.IOException;
+//import java.net.InetSocketAddress;
+//import java.nio.ByteBuffer;
+//import java.nio.channels.SocketChannel;
 import java.util.Scanner;
 import java.io.*;	//InputStreamReaderやBufferedReaderを使えるようにする宣言
 import java.net.*;		//InetAddressやSoket利用のため
+
+ import java.io.BufferedReader;
+ import java.io.IOException;
+ import java.io.InputStreamReader;
+ import java.net.InetSocketAddress;
+ import java.nio.ByteBuffer;
+ import java.nio.CharBuffer;
+ import java.nio.channels.SocketChannel;
 
 /**
  * <p>
@@ -774,6 +782,41 @@ public class API {
     @Document(name="deleteTransaction")
     private AbstractResponse deleteTransactionStatement() throws Exception{
 
+    public static final int ECHO_PORT = 14270;
+    public static final int BUF_SIZE = 1000;
+ 
+    public static void main(String[] args) {
+        SocketChannel channel = null;
+        ByteBuffer buf = ByteBuffer.allocate(BUF_SIZE);
+        Charset charset = Charset.forName("UTF-8");
+        try {
+            channel = SocketChannel.open(new InetSocketAddress("192.168.1.72",
+                                                               ECHO_PORT));
+            BufferedReader keyin = new BufferedReader(new InputStreamReader(System.in));
+            System.out.print("送信：");
+            String line = keyin.readLine();
+            channel.write(charset.encode(CharBuffer.wrap(line + "\n")));
+            while (channel.isConnected()) {
+                buf.clear();
+                if (channel.read(buf) < 0) {
+                    return;
+                }
+                buf.flip();
+                System.out.print("受信：" + charset.decode(buf).toString());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (channel != null && channel.isOpen()) {
+                try {
+                    channel.close();
+                } catch (IOException e) {}
+            }
+        }
+    }
+
+
+/*
         int portNumber = 14270;
         try (
             Socket echoSocket = new Socket("192.168.1.72", portNumber);
@@ -782,10 +825,11 @@ public class API {
 
 
         }
+*/
 
         String name = configuration.isTestnet() ? IRI.TESTNET_NAME : IRI.MAINNET_NAME;
         MilestoneViewModel milestone = MilestoneViewModel.first(tangle);
-        
+
         return GetNodeInfoResponse.create(
             name,
             IotaUtils.getIriVersion(),
